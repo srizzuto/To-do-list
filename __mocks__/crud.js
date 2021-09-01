0e4ae6a5b8bcf70231b4840ecd046bfce7f869d3
Sebastián Rizzuto <srizzuto@Sebastians-Air.fibertel.com.ar>
2021-09-01 14:56:54 -0300

    add test for add fc

--- __mocks__/crud.js
+++ __mocks__/crud.js
@@ -1,13 +1,13 @@
 export const add = (tasks, description) => {
   tasks.push({ description, completed: false, index: tasks.length + 1 });
-  localStorage.setItem('stored', JSON.stringify(tasks));
+  window.localStorage.setItem('stored', JSON.stringify(tasks));
 };
 
 export const updateIndex = (tasks) => {
   tasks.forEach((task) => {
     task.index = tasks.indexOf(task) + 1;
   });
-  localStorage.setItem('stored', JSON.stringify(tasks));
+  window.localStorage.setItem('stored', JSON.stringify(tasks));
 };
 
 export const remove = (tasks, taskToRemove) => {
@@ -21,7 +21,7 @@ export const removeChecked = (tasks) => {
 };
 
 export const edit = (task, description) => {
-  const tasks = JSON.parse(localStorage.getItem('stored'));
+  const tasks = JSON.parse(window.localStorage.getItem('stored'));
   tasks[task.index - 1].description = description;
-  localStorage.setItem('stored', JSON.stringify(tasks));
+  window.localStorage.setItem('stored', JSON.stringify(tasks));
 };
