81d88ec31f09bd39102a5bd7a0e3cfefa631672e
Sebastián Rizzuto <srizzuto@Sebastians-Air.fibertel.com.ar>
2021-09-01 19:38:27 -0300

    fix eslint

--- tests/addremove.test.js
+++ tests/addremove.test.js
@@ -1,4 +1,4 @@
-import { add, edit } from '../src/addremove';
+import { add, edit, remove } from '../src/addremove';
 
 class LocalStorageMock {
   constructor() {
@@ -46,37 +46,37 @@ beforeEach(() => {
 
 describe('The add function', () => {
   test('appends a new task to the store', () => {
-    const description = 'newTask'
+    const description = 'newTask';
     const tasks = JSON.parse(localStorage.getItem('stored'));
     add(tasks, description);
     expect((JSON.parse(localStorage.getItem('stored'))).length).toBe(4);
-  })
+  });
   test('description should be newTask', () => {
-    const description = 'newTask'
+    const description = 'newTask';
     const tasks = JSON.parse(localStorage.getItem('stored'));
     add(tasks, description);
     const task = (JSON.parse(localStorage.getItem('stored')))[3].description;
     expect(task).toBe('newTask');
-  })
-})
+  });
+});
 
 describe('the edit function', () => {
   test('changes the description of the first task', () => {
     const task = (JSON.parse(localStorage.getItem('stored')))[0];
-    edit(task, 'new description')
+    edit(task, 'new description');
     expect((JSON.parse(localStorage.getItem('stored')))[0].description).toBe('new description');
   });
-})
+});
 
 describe('The remove function', () => {
   test('deletes the last task', () => {
     const tasks = JSON.parse(localStorage.getItem('stored'));
     remove(tasks, (JSON.parse(localStorage.getItem('stored')))[2]);
     expect((JSON.parse(localStorage.getItem('stored'))).length).toBe(2);
-  })
+  });
   test('updates the index of the last task after delete', () => {
     const tasks = JSON.parse(localStorage.getItem('stored'));
     remove(tasks, (JSON.parse(localStorage.getItem('stored')))[1]);
     expect(((JSON.parse(localStorage.getItem('stored')))[1]).index).toBe(2);
-  })
-})
+  });
+});
